1a8322bc8b60c9abd65b4c038279a42399e4913c
jomitame <jomitame@gmail.com>
2018-12-16 16:17:14 -0500

    Creando la clase conexion que permite conectarse a una base de datos MySQL

---
+++ src/com/shipis/cjsm/db/Conexion.java
@@ -0,0 +1,35 @@
+package com.shipis.cjsm.db;
+
+import com.shipis.cjsm.settings.Data;
+
+import java.sql.*;
+
+public class Conexion {
+
+    public static Connection getConnection(){
+
+        Connection conn = null;
+
+        try{
+            //Register JDBC driver
+            Class.forName(Data.JDBC_DRIVER);
+            //Open a connection
+            conn = DriverManager.getConnection(Data.DB_URL,Data.USER,Data. PASS);
+
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally {
+            try{
+                if(conn!=null){conn.close();}
+            }catch (SQLException se3){
+                se3.printStackTrace();
+            }
+        }
+
+        return conn;
+    }
+}
