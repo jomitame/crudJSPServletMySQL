404c3bc50e0412d5f07185ac1545a4df5315f0ce
jomitame <jomitame@gmail.com>
2018-12-16 16:19:26 -0500

    Creando el modelo que contiene el método de validacion de usuario

---
+++ src/com/shipis/cjsm/models/LoginUser.java
@@ -0,0 +1,44 @@
+package com.shipis.cjsm.models;
+
+import com.shipis.cjsm.db.Conexion;
+
+import java.sql.*;
+
+public class LoginUser {
+
+    public boolean isValidUserLogin (String pUserName, String pUserPass){
+
+        boolean isValidUser = false;
+
+        Connection con = Conexion.getConnection();
+
+        Statement stat = null;
+        String sql = "";
+
+        try {
+            //making a statement
+            stat = con.createStatement();
+            //creating a query
+            sql = "SELECT * FROM usuarios WHERE nombre = \"" +
+                    pUserName + "\" AND contra = \"" + pUserPass + "\"";
+
+            //Execute a query
+            ResultSet rs = stat.executeQuery(sql);
+
+            // Extract data from resulset
+            if(rs.next()){
+                isValidUser = true;
+            }
+
+            // Clean-up enviroment
+            rs.close();
+            stat.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return isValidUser;
+    }
+
+}
